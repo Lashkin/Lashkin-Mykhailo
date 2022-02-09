941163e38520b28d23e64ea03bf0c1c378848d67
Lashkin <99338027+Lashkin@users.noreply.github.com>
2022-02-09 22:46:38 +0200

    Add files via upload

---
+++ Homework1.3.b/Homework1.3.b/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Homework1._3.b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeVal v1, v2;
+            Swap(out v1, out v2);
+            Console.WriteLine(v1.x);
+            Console.WriteLine(v2.x);
+        }
+
+        private static void Swap(out SomeVal v1, out SomeVal v2)
+        {
+            v1 = new SomeVal();
+            v1.x = Convert.ToString(Console.ReadLine());
+            v2 = v1;
+            v1.x = Convert.ToString(Console.ReadLine());
+        }
+
+        struct SomeVal { public String x; }
+
+
+
+    }
+}
